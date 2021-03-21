150ca6a11f1b8c9dadc93d31bc42ea47c6aeb5b6
ankit sharma <ankitsharmarock99@gmail.com>
2021-03-21 18:24:53 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Ed's Todo List</h1>
+        <h1>Todo List</h1>
       </header>
 
       <Form addTask={(value) => addTodo(value)} setFilter={setFilter} />
